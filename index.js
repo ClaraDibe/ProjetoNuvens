81daf923c5989b1693cc618a6c8a92cf65f268e6
Clara Dibe <claradibe@hotmail.com>
2022-05-17 22:15:38 -0300

    finalmente

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("tchau");
+// Importa o código da biblioteca express
+const express = require("express");
+// Inicializa uma aplicação express
+const app = express();
+// Cria uma função para tratar uma requisição (request handler)
+function index(request, response, next) {
+ response.send("Olá mundo!");
+}
+// Associamos a rota / ao nosso request handler chamado index
+app.get("/", index);
+// Iniciamos a aplicação na porta no valor da variável de ambiente PORT ou 3000
+app.listen(process.env.PORT ?? 3000);
